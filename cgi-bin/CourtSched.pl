c7947a51c837ae454cd20518e8394dcc94423f98
GJordan416 <167732049+GJordan416@users.noreply.github.com>
2024-12-15 01:54:37 -0600

    Create CourtSched.pl

---
+++ cgi-bin/CourtSched.pl
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Court Calendar Error Response</title>
+<style>body {font-family:Arial,Helvetica,sans-serif; font-size:16px;}</style>
+</head>
+<body bgcolor=#ffffff>
+<table style='margin-bottom:30px;' border='0' cellpadding='0' cellspacing='0'>
+<tr><td valign='top' width='154'><img src='/dcourt/assets/logo.png' width='156' height='148' title='U.S. District Court for D.C.' alt='U.S. District Court for D.C.' /></td>
+<td valign='top'><h3>Calendar: Sunday, December 15, 2024</h3></td>
+</tr>
+</table><hr />
+ 
+<b><pre> No Calendar entries found for date(s) selected.
+</pre></b></body>
+</html>
